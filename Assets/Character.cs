a4537700f424a915ad28439166293b9ca15b72a7
Jason <jaepercival@users.noreply.github.com>
2019-01-25 21:43:43 -0500

    added walking

---
+++ Assets/Character.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    Animator animator;
+    Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    bool facingRight = true;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        if (horizontalMovement != 0)
+        {
+            facingRight = (horizontalMovement > 0);
+        }
+
+        spriteRenderer.flipX = !facingRight;
+
+        Vector2 movement = new Vector2(horizontalMovement * speed, verticalMovement * speed);
+        rb.velocity = movement;
+
+        animator.SetFloat("movement", Mathf.Abs(horizontalMovement));
+        
+    }
+}
