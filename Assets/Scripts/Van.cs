724e457205a876d2794bd450507eca60436d9869
Jason <jaepercival@users.noreply.github.com>
2019-01-26 18:52:31 -0500

    van AI

---
+++ Assets/Scripts/Van.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Van : MonoBehaviour
+{
+
+    public float fearIncrease = 0.0005f;
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Character ps = player.GetComponent<Character>();
+
+        var target = player.transform.position - transform.position;
+        if (target.magnitude < 6.0f)
+        {
+            transform.Translate(target * Time.deltaTime * 0.5f);
+            ps.fear += fearIncrease;
+            if (ps.fear > 1.0f)
+            {
+                ps.fear = 1.0f;
+            }
+        }
+        else
+        {
+            ps.fear -= fearIncrease;
+            if (ps.fear < 0)
+            {
+                ps.fear = 0;
+            }
+        }
+    }
+}
