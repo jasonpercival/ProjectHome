7e6cf783b91d150d6803d3135eebee879ebbb39a
Jason <jaepercival@users.noreply.github.com>
2019-01-26 18:21:35 -0500

    re-organized

---
+++ Assets/Scripts/ColliderTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderTest : MonoBehaviour
+{
+    public GameObject spotLight;
+    public FloatingTextManager ftm;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!ftm)
+        {
+            ftm = GameObject.Find("FloatingTextManager").GetComponent<FloatingTextManager>();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            ftm.Show("This doesn't look like the right way to go...", 20, Color.yellow, transform.position, Vector3.up * 25, 4.0f);
+
+            RenderSettings.ambientLight = Color.black;
+            spotLight.SetActive(true);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Home.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Home : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            FloatingTextManager ftm = GameObject.Find("FloatingTextManager").GetComponent<FloatingTextManager>();
+            ftm.Show("The door is locked.", 20, Color.red, transform.position, Vector3.up * 25, 3.0f);
+        }
+    }
+}
